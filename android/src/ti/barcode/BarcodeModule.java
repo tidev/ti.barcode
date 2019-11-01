d8bd80ca1dba3d50874f5b83ef2ac1447428dc10
Chris Williams <chris.a.williams@gmail.com>
2019-11-01 13:35:35 -0400

    fix(android): fix bad import

--- android/src/ti/barcode/BarcodeModule.java
+++ android/src/ti/barcode/BarcodeModule.java
@@ -20,6 +20,7 @@ import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
 import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.client.android.Intents;
